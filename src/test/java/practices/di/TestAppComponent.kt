35c9866a0383cddeeda3179d575a73a0caad7ea3
Haomin <sssurvey@users.noreply.github.com>
2019-11-26 22:35:31 -0600

    Solved: Added solution for TwoSum (#3)

--- src/test/java/practices/di/TestAppComponent.kt
+++ src/test/java/practices/di/TestAppComponent.kt
@@ -2,12 +2,15 @@ package practices.di
 
 import dagger.Component
 import practices.questions.easy.FizzBuzz
+import practices.questions.easy.TwoSum
 import javax.inject.Singleton
 
 @Singleton
 @Component
 interface TestAppComponent {
 
-    fun provideFizzBuzz() : FizzBuzz
+    fun provideFizzBuzz(): FizzBuzz
+
+    fun provideTwoSum(): TwoSum
 
 }
