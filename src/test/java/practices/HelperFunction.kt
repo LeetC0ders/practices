6559ea6dd9b23b64324260325883114a3258f65f
haominshi <haominshi@gmail.com>
2019-11-27 22:21:26 -0600

    Added: Unittest for HelperFunction.intArrayToString

--- src/test/java/practices/HelperFunction.kt
+++ src/test/java/practices/HelperFunction.kt
@@ -2,10 +2,16 @@ package practices
 
 import java.lang.StringBuilder
 
+/**
+ * @param = IntArray
+ * sample return:
+ * passed in: intArrayOf(1, 2, 3)
+ * return in "[1,2,3,]" as String
+ *
+ * sample return, EMPTY intArray
+ * passed in: intArrayOf()
+ * return in "[]" as String
+ */
 fun intArrayToString(intArray: IntArray): String {
-    return StringBuilder().apply {
-        intArray.forEach {
-            append("$it, ")
-        }
-    }.toString()
+    return "[${StringBuilder().apply { intArray.forEach { append("$it,") } }}]"
 }
