bb80023b4b79326534c76363a6fddf1607025d24
Haomin Shi <haomins1@verifone.com>
2020-01-02 09:48:17 -0600

    Cherry picked: added desc for counting bits

--- src/main/java/practices/questions/medium/CountingBits.kt
+++ src/main/java/practices/questions/medium/CountingBits.kt
@@ -5,6 +5,23 @@ import practices.questions.Solution
 import javax.inject.Inject
 import javax.inject.Singleton
 
+/*
+* Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
+*
+* Example 1:
+*
+* Input: 2
+* Output: [0,1,1]
+* Example 2:
+*
+* Input: 5
+* Output: [0,1,1,2,1,2]
+*
+* Follow up:
+* It is very easy to come up with a solution with run time O(n*sizeof(integer)). But can you do it in linear time O(n) /possibly in a single pass?
+* Space complexity should be O(n).
+* Can you do it like a boss? Do it without using any builtin function like __builtin_popcount in c++ or in any other language.
+* */
 @Singleton
 class CountingBits @Inject constructor() : Solution<Int, IntArray> {
     override fun getName() = "CountingBits"
@@ -35,12 +52,12 @@ class CountingBits @Inject constructor() : Solution<Int, IntArray> {
     fun firstAttempt(solutionIn: Int): IntArray {
         var countArr = IntArray((solutionIn + 1))
         var currentNumber = 0
-        for (int in 0 .. solutionIn) {
+        for (int in 0..solutionIn) {
             var binaryString = int.toString(2)
             for (char in binaryString) {
-                when(char) {
+                when (char) {
                     '1' -> countArr[currentNumber]++
-                    else-> Unit
+                    else -> Unit
                 }
             }
             currentNumber++
